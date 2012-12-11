612fef11b1353deef9ae8b03a0c1d5d04334237f
Alex Limpaecher <alimpaecher@gmail.com>
2012-12-11 15:38:53 -0800

    Now only good drawings are grabbed.
    The test drawings are removed from the database

--- dbUtils/drawAFriendMetaData.m
+++ dbUtils/drawAFriendMetaData.m
@@ -14,5 +14,19 @@ function [ objectID, drawerID, guesserID, modelID, newset, accuracy ] = drawAFri
     modelID = C{5};
     newset = strcmp('(null)',C{7});
     accuracy = cell2mat(C{8});
+    
+    removeGame(objectID, drawerID, guesserID, modelID, newset, accuracy, 100003121308246, 1103941);
 end
+
+function [ objectID, drawerID, guesserID, modelID, newset, accuracy] = removeGame( objectID, drawerID, guesserID, modelID, newset, accuracy, player1, player2)
+    gameIndex = (player1 == drawerID & player2 == guesserID) | (player2 == drawerID & player1 == guesserID);
+    objectID = objectID(~gameIndex,:);
+    drawerID = drawerID(~gameIndex,:);
+    guesserID = guesserID(~gameIndex,:);
+    modelID = modelID(~gameIndex,:);
+    newset = newset(~gameIndex,:);
+    accuracy = accuracy(~gameIndex,:);
+end
+
+
 
