2e18aad44258c50e314ae6770e1c22461336e4ad
Nicolas Feltman <nfeltman@gmail.com>
2012-12-15 22:38:26 -0500

    Added a skeleton for runOnValidEntries.

---
+++ dbUtils/runOnValidEntries.m
@@ -0,0 +1,13 @@
+function output = runOnValidEntries( f, dbFile )
+%RUNONVALIDIMAGES Runs a function on all valid images in the database.
+%   
+%   runOnValidEntries( f, dbFile )
+%       Runs the function f on every valid image in the database file
+%       identified by dbFile.  The results of f, for every image, are
+%       returned as a cell array.
+
+
+error('Not implmented yet!');
+
+end
+
