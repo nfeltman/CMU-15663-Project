4454af60a408e6eb20fa03cba34164a9c5013f38
Nicolas Feltman <nfeltman@gmail.com>
2013-01-10 17:03:08 -0500

    Cleaning, and changed size.

--- exampleScripts/calculateWholeFields.m
+++ exampleScripts/calculateWholeFields.m
@@ -4,13 +4,16 @@ targets = ['4R56rYWZfQ';'9ggZFhGnnO';'E5znYs8nrI';'dpzhjaPg5T'; 'qUeZYAL0IZ'; ..
     'EaevUQv9A7';'Hss07oWTUY';'mobldcT3bY';'9zkRrokirT';'Syt9xsCCzO'; ...
     'u1uPtY8qjV';'yOijC9dBmf';'Zety8LiUdt'];
 
-[gridX,gridY] = meshgrid(1:350,1:500);
-grid = [mtov(gridX), mtov(gridY)];
-[X, Y] = findAllDeltas(grid, targets);
+% [gridX,gridY] = meshgrid(1:320,1:460);
+% grid = [mtov(gridX), mtov(gridY)];
+% [X, Y] = findAllDeltas(grid, targets);
 [avg, smartAvg, score] = findSmartAverage(X,Y);
+magAvg = sqrt(sum(avg.*avg,2));
 
-scoreIm = reshape(score,500,350);
+magIm = reshape(magAvg,460,320);
+scoreIm = reshape(score,460,320);
 
 figure(3);
 imagesc(scoreIm);
 fixAxes();
+colormap jet
--- moveToAverage/findOptimumCone.m
+++ moveToAverage/findOptimumCone.m
@@ -2,34 +2,6 @@ function [ U, score ] = findOptimumCone( X, Y )
 %FINDOPTIMUMCONE Summary of this function goes here
 %   Detailed explanation goes here
 
-n = size(X,1);
-alpha = 6;
-beta = 2;
-
-denom = sqrt(X.*X+Y.*Y) + beta;
-o = ones(1,size(X,2));
-
-% bestScore = inf;
-% ux = ones(n,1);
-% uy = sqrt(1-ux.*ux);
-% for k = 1:10,
-%     
-%     dotProd = (ux*o).*X + (uy*o).*Y;
-%     scores = 1-(abs(dotProd)./denom);
-%     towardZero = 1-(dotProd > 0)*2;
-%     factor = alpha*scores.^(alpha-1).*towardZero./denom;
-%     gux = sum(X.*factor,2);
-%     guy = sum(Y.*factor,2);
-%     
-%     score = sum(scores,2)
-%     
-%     ux = ux + gux*0.5;
-%     uy = uy + guy*0.5;
-%     len = sqrt(ux.*ux+uy.*uy);
-%     ux = ux./len;
-%     uy = uy./len;
-% end
-
 a = sum(X.*X,2);
 b = sum(X.*Y,2);
 c = sum(Y.*Y,2);
@@ -38,13 +10,30 @@ smaller_eig = (a+c - sqrt((a-c).^2+4*b.*b)) / 2;
 
 ux = a - smaller_eig;
 uy = b;
-len = sqrt(ux.*ux+uy.*uy);
-ux = ux./len;
-uy = uy./len;
+uLen = sqrt(ux.*ux+uy.*uy);
+ux = ux./uLen;
+uy = uy./uLen;
 U = [ux, ux];
 
+
+alpha = 6;
+beta = 2;
+
+o = ones(1,size(X,2));
 dotProd = (ux*o).*Y - (uy*o).*X;
+denom = sqrt(X.*X+Y.*Y) + beta;
 score = mean((1-abs(dotProd)./denom).^alpha,2);
+
+% 
+% alpha = 0.1;
+% beta = 2;
+% epsilon = 2;
+% 
+% o = ones(1,size(X,2));
+% vLen = sqrt(X.*X+Y.*Y);
+% numer = 1-abs(((ux*o).*Y - (uy*o).*X)./vLen);
+% denom = alpha*(vLen.^beta) + epsilon;
+% score = mean(numer./denom,2);
 
 end
 
